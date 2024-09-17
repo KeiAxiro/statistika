4ba034192232594b89eab696e90c26a2ac2df394
KeiAxiro <makfaruk6@gmail.com>
2024-09-17 11:41:59 +0700

    semuaa ajaa capek guaaa

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    file: 'home' 
+  res.render('home', {
+    title: 'Home'
   });
 });
 
